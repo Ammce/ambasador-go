f640aeb6a0b57c3268ccf4bf8c1385be69638565
Amel Muminovic <amel@yulife.com>
2021-11-02 20:04:57 +0100

    User collection added

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
+	"github.com/Ammce/ambasador-go/src/database"
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
 
-	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambasador"), &gorm.Config{})
-
-	if err != nil {
-		panic("Could not connect with the database")
-	}
+	database.Connect()
+	database.AutoMigrate()
 
 	app := fiber.New()
 
---
+++ src/database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"github.com/Ammce/ambasador-go/src/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambasador"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect with the database")
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{})
+}
