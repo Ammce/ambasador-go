d39a6235c9dfdc5312f3db0ff2f3fde0067ae09e
Amel Muminovic <amel@yulife.com>
2021-11-02 20:32:49 +0100

    Registering user

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/Ammce/ambasador-go
 go 1.17
 
 require (
+	github.com/gofiber/fiber/v2 v2.21.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	gorm.io/driver/mysql v1.1.3
+	gorm.io/gorm v1.22.2
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/gofiber/fiber/v2 v2.21.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
@@ -13,6 +19,4 @@ require (
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20211102061401-a2f17f7b995c // indirect
-	gorm.io/driver/mysql v1.1.3 // indirect
-	gorm.io/gorm v1.22.2 // indirect
 )
--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -1,9 +1,40 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+
+	"github.com/Ammce/ambasador-go/src/database"
+	"github.com/Ammce/ambasador-go/src/models"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Hello",
-	})
+
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(http.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Password do not match",
+		})
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
+
+	user := models.User{
+		FirstName:   data["first_name"],
+		LastName:    data["last_name"],
+		Email:       data["email"],
+		IsAmbasador: false,
+		Password:    password,
+	}
+
+	database.DB.Create(&user)
+
+	return c.JSON(&user)
 }
