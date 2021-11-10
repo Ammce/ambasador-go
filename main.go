3bdc6a58f622faa7cf9d9c424b815a0146b0b64f
Amel Muminovic <amcenp@gmail.com>
2021-11-10 01:59:13 +0100

    Logger added

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/Ammce/ambasador-go/src/database"
+	"github.com/Ammce/ambasador-go/src/middlewares"
 	"github.com/Ammce/ambasador-go/src/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -17,6 +18,9 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 	}))
+
+	middlewares.Logger(app)
+
 	routes.Setup(app)
 
 	app.Listen(":8080")
