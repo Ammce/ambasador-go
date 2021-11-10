8a6dbc9d929520da1fe9749cb070e928c48100b8
Amel Muminovic <amcenp@gmail.com>
2021-11-10 19:47:34 +0100

    Create and update product

---
+++ src/controllers/productController.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"strconv"
+
+	"github.com/Ammce/ambasador-go/src/database"
+	"github.com/Ammce/ambasador-go/src/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Products(c *fiber.Ctx) error {
+	var products []models.Product
+
+	database.DB.Find(&products)
+
+	c.Status(fiber.StatusOK)
+	return c.JSON(fiber.Map{
+		"data": products,
+	})
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Create(&product)
+
+	return c.JSON(fiber.Map{
+		"data": product,
+	})
+}
+
+func Product(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	var product models.Product
+
+	product.Id = uint(id)
+
+	database.DB.Find(&product)
+
+	return c.JSON(fiber.Map{
+		"data": product,
+	})
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	priceFloat, _ := strconv.ParseFloat(data["price"], 64)
+
+	product := models.Product{
+		Id:          uint(id),
+		Title:       data["title"],
+		Description: data["description"],
+		Price:       priceFloat,
+		Image:       data["image"],
+	}
+
+	database.DB.Find(&product)
+
+	return c.JSON(fiber.Map{
+		"data": product,
+	})
+}
